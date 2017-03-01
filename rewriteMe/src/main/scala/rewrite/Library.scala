e08d2546aac7c6ca1a7c239276adc9eb88382b4b
Ólafur Páll Geirsson <olafurpg@gmail.com>
2017-03-01 18:42:52 +0100

    Implement filter => find rewrite

--- rewriteMe/src/main/scala/rewrite/Library.scala
+++ rewriteMe/src/main/scala/rewrite/Library.scala
@@ -1,6 +1,8 @@
 package rewrite
 object Library {
   val lst = Seq(1, 2, 2)
+  lst.filter(_ > 2 /* this is a comment */)
+      /* not joking */ . /* pathological */ headOption
   def main(args: Array[String]) {
     lst.foreach(println)
   }
--- rewritesHere/src/main/scala/rewrite/MyRewrite.scala
+++ rewritesHere/src/main/scala/rewrite/MyRewrite.scala
@@ -1,9 +1,22 @@
 package rewrite
 import scalafix._, rewrite._, util._, config._, TreePatch._, TokenPatch._
-import scala.meta._, semantic._, contrib._
+import scala.meta._, semantic._, contrib._, Token._
 import scala.collection.immutable.Seq
 
 object MyRewrite {
+
+  val filterHeadOptionToFind: Rewrite[Mirror] = Rewrite[Mirror] { ctx =>
+    import ctx._
+    implicit val mirror = ctx.mirror
+    tree.collect {
+      case q"$_.$filter($cond).$headOption"
+          if filter.syntax == "filter" && headOption.syntax == "headOption" =>
+        List(Rename(filter, q"find")) ++
+          headOption.tokens(ctx.config.dialect).map(Remove.apply) ++
+          tokenList.find(cond.tokens.last)(_.is[Dot]).toList.map(Remove.apply)
+    }.flatten
+  }
+
   val seqToList: Rewrite[Mirror] = Rewrite[Mirror] { ctx =>
     import ctx._
     implicit val mirror = ctx.mirror
@@ -32,7 +45,7 @@ object MyRewrite {
     logger.elem(mirror.database)
     mirror.sources.foreach { source =>
       implicit val ctx = RewriteCtx(source, config, mirror)
-      val patches = seqToList.rewrite(ctx)
+      val patches = filterHeadOptionToFind.rewrite(ctx)
       val patched = Patch(patches)
       logger.elem(source, patched)
     }
