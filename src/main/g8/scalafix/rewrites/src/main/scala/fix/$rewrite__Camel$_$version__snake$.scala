dc1c714e756a3fb060471e013da3435524b373bb
Ólafur Páll Geirsson <olafurpg@gmail.com>
2017-08-12 00:40:30 +0200

    s/debugMirror/debugSemanticCtx/

--- src/main/g8/scalafix/rewrites/src/main/scala/fix/$rewrite__Camel$_$version__snake$.scala
+++ src/main/g8/scalafix/rewrites/src/main/scala/fix/$rewrite__Camel$_$version__snake$.scala
@@ -5,7 +5,7 @@ import scala.meta._
 
 case class $rewrite;format="Camel"$_$version;format="snake"$(sctx: SemanticCtx) extends SemanticRewrite(sctx) {
   def rewrite(ctx: RewriteCtx): Patch = {
-    ctx.debugMirror()
+    ctx.debugSemanticCtx()
     ctx.debug(ctx.tree.syntax)
     ctx.debug(ctx.tree.structure)
     Patch.empty
