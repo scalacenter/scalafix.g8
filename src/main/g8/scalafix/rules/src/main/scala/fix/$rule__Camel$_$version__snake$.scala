06f584b25275b8ecaa0d19f04d84f752034bdf20
Ólafur Páll Geirsson <olafurpg@gmail.com>
2017-09-07 15:11:15 +0200

    Remove dollar signs, g8 can't handle them.

--- src/main/g8/scalafix/rules/src/main/scala/fix/$rule__Camel$_$version__snake$.scala
+++ src/main/g8/scalafix/rules/src/main/scala/fix/$rule__Camel$_$version__snake$.scala
@@ -6,8 +6,8 @@ import scala.meta._
 case class $rule;format="Camel"$_$version;format="snake"$(index: SemanticdbIndex) extends SemanticRule(index, "$rule;format="Camel"$_$version;format="snake"$") {
   override def fix(ctx: RewriteCtx): Patch = {
     ctx.debugIndex()
-    println(s"Tree.syntax: ${ctx.tree.syntax}")
-    println(s"Tree.structure: ${ctx.tree.structure}")
+    println(s"Tree.syntax: " + ctx.tree.syntax)
+    println(s"Tree.structure: " + ctx.tree.structure)
     Patch.empty
   }
 }
