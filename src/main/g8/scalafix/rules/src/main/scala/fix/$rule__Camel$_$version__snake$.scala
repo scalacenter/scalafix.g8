5cfae4d02a42f0dc1c2a51f2a110d441feb2f908
Dale Wijnand <344610+dwijnand@users.noreply.github.com>
2017-09-19 13:42:21 +0100

    final case class, and wrap

--- src/main/g8/scalafix/rules/src/main/scala/fix/$rule__Camel$_$version__snake$.scala
+++ src/main/g8/scalafix/rules/src/main/scala/fix/$rule__Camel$_$version__snake$.scala
@@ -3,11 +3,14 @@ package fix
 import scalafix._
 import scala.meta._
 
-case class $rule;format="Camel"$_$version;format="snake"$(index: SemanticdbIndex) extends SemanticRule(index, "$rule;format="Camel"$_$version;format="snake"$") {
+final case class $rule;format="Camel"$_$version;format="snake"$(index: SemanticdbIndex)
+    extends SemanticRule(index, "$rule;format="Camel"$_$version;format="snake"$") {
+
   override def fix(ctx: RuleCtx): Patch = {
     ctx.debugIndex()
     println(s"Tree.syntax: " + ctx.tree.syntax)
     println(s"Tree.structure: " + ctx.tree.structure)
     Patch.empty
   }
+
 }
