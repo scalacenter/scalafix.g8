65ac5ed7a2003af4a9278b2da1ce699869e3af52
Brice Jaglin <bjaglin@teads.tv>
2021-05-20 11:21:40 +0200

    check CI failure

--- migration-rules/v0.9.28/output/src/main/scala/fix/OldSemanticRule.scala
+++ migration-rules/v0.9.28/output/src/main/scala/fix/OldSemanticRule.scala
@@ -1,8 +1,7 @@
 package fix
 
-import org.scalatest.FunSuiteLike
 import scalafix.testkit.AbstractSemanticRuleSuite
 
-class OldSemanticRule extends AbstractSemanticRuleSuite with FunSuiteLike {
+class OldSemanticRule extends AbstractSemanticRuleSuite {
   runAllTests()
 }
