5bff0bb81f047b6f2b95e3528ce559b4f1947e06
Meriam Lachkar <meriam.lachkar@gmail.com>
2021-05-21 07:49:25 +0200

    Change the rule

--- migration-rules/v0.9.28/output/src/main/scala/fix/OldSemanticRule.scala
+++ migration-rules/v0.9.28/output/src/main/scala/fix/OldSemanticRule.scala
@@ -6,3 +6,6 @@ import scalafix.testkit.AbstractSemanticRuleSuite
 class OldSemanticRule extends AbstractSemanticRuleSuite with FunSuiteLike {
   runAllTests()
 }
+class OldSemanticRule2 extends scalafix.testkit.AbstractSemanticRuleSuite with FunSuiteLike {
+  runAllTests()
+}
