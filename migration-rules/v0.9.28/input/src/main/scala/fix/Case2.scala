ad226e444ebbda5adb3ef6496ed7db44ac8cfdc5
Meriam Lachkar <meriam.lachkar@gmail.com>
2021-05-21 11:04:42 +0200

    fix another case

--- migration-rules/v0.9.28/input/src/main/scala/fix/Case2.scala
+++ migration-rules/v0.9.28/input/src/main/scala/fix/Case2.scala
@@ -12,6 +12,10 @@ trait BeforeAndAfterAllConfigMapAlt {
 }
 class RuleSuite extends SemanticRuleSuite with BeforeAndAfterAllConfigMapAlt {
   val isSaveExpectField = ensureAccessible(classOf[SemanticRuleSuite].getDeclaredField("isSaveExpect"))
+  val isSaveExpectField2 = ensureAccessible(classOf[scalafix.testkit.SemanticRuleSuite].getDeclaredField("isSaveExpect"))
+  val fake = classOf[Int]
+  val fake2 = 1.asInstanceOf[SemanticRuleSuite]
+  val fake3 = 1.asInstanceOf[scalafix.testkit.SemanticRuleSuite]
 
   runAllTests()
 }
--- migration-rules/v0.9.28/input/src/main/scala/fix/OldSemanticRule.scala
+++ migration-rules/v0.9.28/input/src/main/scala/fix/OldSemanticRule.scala
@@ -8,6 +8,12 @@ import scalafix.testkit.SemanticRuleSuite
 class OldSemanticRule extends SemanticRuleSuite() {
   runAllTests()
 }
+class OldSemanticRule1 extends SemanticRuleSuite {
+  runAllTests()
+}
 class OldSemanticRule2 extends scalafix.testkit.SemanticRuleSuite {
+  runAllTests()
+}
+class OldSemanticRule3 extends scalafix.testkit.SemanticRuleSuite() {
   runAllTests()
 }
